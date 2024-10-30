a54b771d70cc5da76f16fd816e39374aa32c5bce
Félix Galindo <Felix Galindo>
2024-10-30 17:21:49 -0400

    main > fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React, { ReactNode } from "react";
 import Sidebar from "./components/sidebar";
 import "./app.scss";
+import Profile from "./views/profile/profile";
+import Technologies from "./views/technologies/technologies";
+import Works from "./views/works/works";
+import Projects from "./views/projects/projects";
+import Academy from "./views/academy/academy";
+import Certificates from "./views/certificates/certificates";
+import { Route, Routes } from "react-router";
 
 type LayoutProps = {
   children?: ReactNode;
 };
 
-const App: React.FC<LayoutProps> = ({ children }) => {
+const App: React.FC<LayoutProps> = () => {
   return (
     <div className="app-container">
       <Sidebar />
       <div className="app-container-body">
         <h1 className="app-container-body-header">Web Developer Full Stack</h1>
-        {children}
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/technologies" element={<Technologies />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/academy" element={<Academy />} />
+          <Route path="/certificates" element={<Certificates />} />
+          <Route path="*" element={<Profile />} />
+        </Routes>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,47 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Profile from "./views/profile/profile";
-import Technologies from "./views/technologies/technologies";
-import Works from "./views/works/works";
-import Projects from "./views/projects/projects";
-import Academy from "./views/academy/academy";
-import Certificates from "./views/certificates/certificates";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App children={<Profile />} />,
-  },
-  {
-    path: "/technologies",
-    element: <App children={<Technologies />} />,
-  },
-  {
-    path: "/works",
-    element: <App children={<Works />} />,
-  },
-  {
-    path: "/projects",
-    element: <App children={<Projects />} />,
-  },
-  {
-    path: "/academy",
-    element: <App children={<Academy />} />,
-  },
-  {
-    path: "/certificates",
-    element: <App children={<Certificates />} />,
-  },
-]);
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <App>
+          <Profile />
+        </App>
+      </BrowserRouter>
     </StrictMode>
   );
 }
