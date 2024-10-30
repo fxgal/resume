e7dc9982b837f9cba072a7d0671c2af7280f3be0
Félix Galindo <Felix Galindo>
2024-10-30 17:22:41 -0400

    main > fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/resume",
+  base: "https://fxgal.github.io/resume",
   plugins: [react()],
   css: {
     preprocessorOptions: {
