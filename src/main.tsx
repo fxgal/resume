02d446f70ffbaedaf0349d3d184d364b4c74e2f7
Félix Galindo <Felix Galindo>
2024-11-23 18:52:04 -0400

    main > languages

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Profile from "./views/profile/profile";
+import "./i18n/i18n";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
